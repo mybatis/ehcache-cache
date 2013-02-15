5f4e4e227d69b0c4febd3bec068726583caa47f7
Simo Tripodi <stripodi@adobe.com>
2013-02-15 19:43:39 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.caches.ehcache", "org/mybatis/caches/ehcache/package-summary.html", [ [ "Description", "org/mybatis/caches/ehcache/package-descr.html", null, "" ], [ "Classes", null, [ [ "EhcacheCache", "org/mybatis/caches/ehcache/EhcacheCache.html", null, "" ], [ "LoggingEhcache", "org/mybatis/caches/ehcache/LoggingEhcache.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,6 @@
+var DATA = [
+      { id:0, label:"org.mybatis.caches.ehcache", link:"org/mybatis/caches/ehcache/package-summary.html", type:"package" },
+      { id:1, label:"org.mybatis.caches.ehcache.EhcacheCache", link:"org/mybatis/caches/ehcache/EhcacheCache.html", type:"class" },
+      { id:2, label:"org.mybatis.caches.ehcache.LoggingEhcache", link:"org/mybatis/caches/ehcache/LoggingEhcache.html", type:"class" }
+
+    ];
