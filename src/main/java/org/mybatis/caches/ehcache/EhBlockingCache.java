7f3f6ceaca816a5318fb4f3aa1cedb0630d22bf8
Eduardo Macarron <eduardo.macarron@gmail.com>
2015-05-01 17:12:49 +0200

    Fixes #15. Blocking cache support (missing resource)

---
+++ src/main/java/org/mybatis/caches/ehcache/EhBlockingCache.java
@@ -0,0 +1,46 @@
+/*
+ *    Copyright 2010-2014 The original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.mybatis.caches.ehcache;
+
+import java.util.concurrent.locks.ReadWriteLock;
+
+import net.sf.ehcache.Ehcache;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.constructs.blocking.BlockingCache;
+
+/**
+ * 
+ * @author Iwao AVE!
+ *
+ */
+public class EhBlockingCache extends AbstractEhcacheCache {
+
+  public EhBlockingCache(final String id) {
+    super(id);
+    if (!CACHE_MANAGER.cacheExists(id)) {
+      CACHE_MANAGER.addCache(this.id);
+      Ehcache ehcache = CACHE_MANAGER.getEhcache(this.id);
+      BlockingCache blockingCache = new BlockingCache(ehcache);
+      CACHE_MANAGER.replaceCacheWithDecoratedCache(ehcache, blockingCache);
+    }
+  }
+
+  @Override
+  public void putObject(Object key, Object value) {
+    cache.put(new Element(key, value));
+  }
+    
+}
