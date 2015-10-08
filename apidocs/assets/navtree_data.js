9d2ce3b26b115aa8a0143c80b323c92d435bfdbb
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-08 00:43:16 -0400

    wagon-git commit

--- apidocs/assets/navtree_data.js
+++ apidocs/assets/navtree_data.js
@@ -1,5 +1,5 @@
 var NAVTREE_DATA =
-[ [ "org.mybatis.caches.ehcache", "org/mybatis/caches/ehcache/package-summary.html", [ [ "Description", "org/mybatis/caches/ehcache/package-descr.html", null, "" ], [ "Classes", null, [ [ "EhcacheCache", "org/mybatis/caches/ehcache/EhcacheCache.html", null, "" ], [ "LoggingEhcache", "org/mybatis/caches/ehcache/LoggingEhcache.html", null, "" ] ]
+[ [ "org.mybatis.caches.ehcache", "org/mybatis/caches/ehcache/package-summary.html", [ [ "Classes", null, [ [ "AbstractEhcacheCache", "org/mybatis/caches/ehcache/AbstractEhcacheCache.html", null, "" ], [ "EhBlockingCache", "org/mybatis/caches/ehcache/EhBlockingCache.html", null, "" ], [ "EhcacheCache", "org/mybatis/caches/ehcache/EhcacheCache.html", null, "" ], [ "LoggingEhcache", "org/mybatis/caches/ehcache/LoggingEhcache.html", null, "" ] ]
 , "" ] ]
 , "" ] ]
 
--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -1,6 +1,8 @@
 var DATA = [
       { id:0, label:"org.mybatis.caches.ehcache", link:"org/mybatis/caches/ehcache/package-summary.html", type:"package" },
-      { id:1, label:"org.mybatis.caches.ehcache.EhcacheCache", link:"org/mybatis/caches/ehcache/EhcacheCache.html", type:"class" },
-      { id:2, label:"org.mybatis.caches.ehcache.LoggingEhcache", link:"org/mybatis/caches/ehcache/LoggingEhcache.html", type:"class" }
+      { id:1, label:"org.mybatis.caches.ehcache.AbstractEhcacheCache", link:"org/mybatis/caches/ehcache/AbstractEhcacheCache.html", type:"class" },
+      { id:2, label:"org.mybatis.caches.ehcache.EhBlockingCache", link:"org/mybatis/caches/ehcache/EhBlockingCache.html", type:"class" },
+      { id:3, label:"org.mybatis.caches.ehcache.EhcacheCache", link:"org/mybatis/caches/ehcache/EhcacheCache.html", type:"class" },
+      { id:4, label:"org.mybatis.caches.ehcache.LoggingEhcache", link:"org/mybatis/caches/ehcache/LoggingEhcache.html", type:"class" }
 
     ];
