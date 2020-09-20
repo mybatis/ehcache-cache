26de36b208b3e41cd36deb5a56638ea57ed99950
Jeremy Landis <jeremylandis@hotmail.com>
2020-09-20 14:58:36 -0400

    [ci] Formatting / update license header

--- src/test/java/org/mybatis/caches/ehcache/EhBlockingCacheTestCase.java
+++ src/test/java/org/mybatis/caches/ehcache/EhBlockingCacheTestCase.java
@@ -16,11 +16,12 @@
 package org.mybatis.caches.ehcache;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
