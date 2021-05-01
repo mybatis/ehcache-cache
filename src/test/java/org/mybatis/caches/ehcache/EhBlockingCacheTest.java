582f0431c303682396488fa58e0aa4096c7805d8
Jeremy Landis <jeremylandis@hotmail.com>
2021-05-01 13:34:08 -0400

    [tests] Properly name the test class files

--- src/test/java/org/mybatis/caches/ehcache/EhBlockingCacheTest.java
+++ src/test/java/org/mybatis/caches/ehcache/EhBlockingCacheTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public final class EhBlockingCacheTestCase {
+public final class EhBlockingCacheTest {
 
   private static final String DEFAULT_ID = "EHBLOCKINGCACHE";
 
--- src/test/java/org/mybatis/caches/ehcache/EhcacheTest.java
+++ src/test/java/org/mybatis/caches/ehcache/EhcacheTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public final class EhcacheTestCase {
+public final class EhcacheTest {
 
   private static final String DEFAULT_ID = "EHCACHE";
 
