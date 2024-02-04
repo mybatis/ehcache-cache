5011fe71549f4330edba5c89eec086eda5841bf9
hazendaz <jeremylandis@hotmail.com>
2024-02-04 20:18:22 +0000

    Deploying to gh-pages from @ mybatis/ehcache-cache@da9757325cf3678b8e2d36e166446bd4b4933b80 🚀

--- apidocs/assets/navtree_data.js
+++
@@ -1,7 +0,0 @@
-var NAVTREE_DATA =
-[ [ "org.mybatis.caches.ehcache", "org/mybatis/caches/ehcache/package-summary.html", [ [ "Classes", null, [ [ "AbstractEhcacheCache", "org/mybatis/caches/ehcache/AbstractEhcacheCache.html", null, "" ], [ "EhBlockingCache", "org/mybatis/caches/ehcache/EhBlockingCache.html", null, "" ], [ "EhcacheCache", "org/mybatis/caches/ehcache/EhcacheCache.html", null, "" ], [ "LoggingEhcache", "org/mybatis/caches/ehcache/LoggingEhcache.html", null, "" ] ]
-, "" ] ]
-, "" ] ]
-
-;
-
