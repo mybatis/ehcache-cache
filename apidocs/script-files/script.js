1de597f8596b737fb86897b5f89b06c9b84134b7
Jeremy Landis <jeremylandis@hotmail.com>
2024-09-15 18:37:57 -0400

    Site checkin for project mybatis-ehcache

--- apidocs/script-files/script.js
+++ apidocs/script-files/script.js
@@ -1,26 +1,26 @@
 /*
  * Copyright (c) 2013, 2023, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
  *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
  *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  */
 
 var moduleSearchIndex;
@@ -37,7 +37,7 @@ var tableTab = "table-tab";
 var activeTableTab = "active-table-tab";
 
 function loadScripts(doc, tag) {
-    createElem(doc, tag, 'search.js');
+    createElem(doc, tag, 'script-files/search.js');
 
     createElem(doc, tag, 'module-search-index.js');
     createElem(doc, tag, 'package-search-index.js');
@@ -157,7 +157,7 @@ function show(tableId, selected, columns) {
 }
 
 function updateTabs(tableId, selected) {
-    document.getElementById(tableId + '.tabpanel')
+    document.querySelector('div#' + tableId +' .summary-table')
         .setAttribute('aria-labelledby', selected);
     document.querySelectorAll('button[id^="' + tableId + '"]')
         .forEach(function(tab, index) {
@@ -227,27 +227,10 @@ function switchCopyLabel(button, span) {
         }, 100);
     }, 1900);
 }
-// Workaround for scroll position not being included in browser history (8249133)
+// Dynamically set scroll margin to accomodate for draft header
 document.addEventListener("DOMContentLoaded", function(e) {
-    var contentDiv = document.querySelector("div.flex-content");
-    window.addEventListener("popstate", function(e) {
-        if (e.state !== null) {
-            contentDiv.scrollTop = e.state;
-        }
-    });
-    window.addEventListener("hashchange", function(e) {
-        history.replaceState(contentDiv.scrollTop, document.title);
-    });
-    var timeoutId;
-    contentDiv.addEventListener("scroll", function(e) {
-        if (timeoutId) {
-            clearTimeout(timeoutId);
-        }
-        timeoutId = setTimeout(function() {
-            history.replaceState(contentDiv.scrollTop, document.title);
-        }, 100);
-    });
-    if (!location.hash) {
-        history.replaceState(contentDiv.scrollTop, document.title);
-    }
+    document.querySelectorAll(':not(input)[id]').forEach(
+        function(c) {
+            c.style["scroll-margin-top"] = Math.ceil(document.querySelector("header").offsetHeight) + "px"
+        });
 });
