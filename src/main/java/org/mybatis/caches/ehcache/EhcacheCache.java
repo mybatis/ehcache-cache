703b2095fcef9d9c54b536be7c6a11087eb3441e
Jeremy Landis <jeremylandis@hotmail.com>
2026-03-08 20:44:28 -0400

    Pull forward code from upstream master

--- src/main/java/org/mybatis/caches/ehcache/EhcacheCache.java
+++ src/main/java/org/mybatis/caches/ehcache/EhcacheCache.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2010-2022 the original author or authors.
+ *    Copyright 2010-2026 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,6 +15,9 @@
  */
 package org.mybatis.caches.ehcache;
 
+/**
+ * Cache implementation backed by Ehcache 2.
+ */
 public class EhcacheCache extends AbstractEhcacheCache {
 
   /**
